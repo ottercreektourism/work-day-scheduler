d6545e07bb5a05dde4db486a32fd88365b49a68f
Marissa Flynn <mkflynn13@gmail.com>
2022-01-25 13:15:04 -0500

    Added colorTime function to color code a time block, plus a for loop to loop through all time blocks.

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,19 +21,22 @@ $(".saveBtn").click(function (event) {
     var hourId = $(this).parent().attr("id");
     // console.log(hourId);
     localStorage.setItem(hourId, saveData);
-    getSchedule();
+    
 });
+
 function getSchedule() {
-    $("#9 .description").val(locaStorage.getItem("9am"));
-    $("#10 .description").val(locaStorage.getItem("10am"));
-    $("#11 .description").val(locaStorage.getItem("11am"));
-    $("#12 .description").val(locaStorage.getItem("12am"));
-    $("#1 .description").val(locaStorage.getItem("1pm"));
-    $("#2 .description").val(locaStorage.getItem("2pm"));
-    $("#3 .description").val(locaStorage.getItem("3pm"));
-    $("#4 .description").val(locaStorage.getItem("4pm"));
-    $("#5 .description").val(locaStorage.getItem("5pm"));
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#1 .description").val(localStorage.getItem("1"));
+    $("#2 .description").val(localStorage.getItem("2"));
+    $("#3 .description").val(localStorage.getItem("3"));
+    $("#4 .description").val(localStorage.getItem("4"));
+    $("#5 .description").val(localStorage.getItem("5"));
 };
+
+getSchedule();
 
 
 var dateTrackerElement = $('#currentDay');
@@ -42,37 +45,36 @@ function timeTracker() {
     var currentDay = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
     dateTrackerElement.text(currentDay);
     // dateTrackerElement.textContent = currentDay;
-    colorTime();
 
 }
 setInterval(timeTracker, 1000);
 
-// var currentTime = moment().format('H');
-var time9 = $('#9');
-var time10 = 10;
-var time11 = 11;
-var time12 = 12;
-var time1 = 13;
-var time2 = 14;
-var time3 = 15;
-var time4 = 16;
-var time5 = 17;
+var currentTime = moment().hours();
 
-function colorTime() {
-    if (currentTime == time9) {
-        $("#9 .description").addclass("present");
-    } else if (currentTime < time9) {
-        $("#9 .description").addclass("past");
+var time9 = $('#hour-9');
+var time10 = $('#hour-10');
+var time11 = $('#hour-11');
+var time12 = $('#hour-12');
+var time13 = $('#hour-13');
+var time14 = $('#hour-14');
+var time15 = $('#hour-15');
+var time16 = $('#hour-16');
+var time17 = $('#hour-17');
+var times = [time9,time10, time11, time12, time13, time14, time15, time16, time17]
+
+function colorTime(currentHour) {
+    console.log(currentHour);
+    var hour = parseInt(currentHour.attr('id').split("-")[1])
+    if (currentTime == hour) {
+        currentHour.addClass("present");
+    } else if (currentTime > hour) {
+        currentHour.addClass("past");
     } else {
-        $("#9 .description").addclass("future");
-    }
-    
-    if (currentTime == time10) {
-        $("#10 .description").addclass("present");
-    } else if (currentTime < time10) {
-        $("#10 .description").addclass("past");
-    } else {
-        $("#10 .description").addclass("future");
+        currentHour.addClass("future");
     }
 };
+// colorTime(time9);
+for (let index = 0; index < times.length; index++) {
+    colorTime(times[index]);
+}
 
